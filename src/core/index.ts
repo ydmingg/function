c753d071e52f01ae708f8441889ce2fd50d261ec
ydming <xm95zz@gmail.com>
2024-08-14 17:54:16 +0800

    init

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,11 +3,16 @@ import UI from "../ui";
 export default class Core { 
     ui: UI
     constructor() { 
-        // 加载ui
+        // 初始化进度条
+        
+
+
+
+        // 实例化ui
         this.ui = new UI();
         
     }
 
-    
+
     
 }
